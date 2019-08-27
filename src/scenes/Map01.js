7adbaaab0399801c0f359f9880b8747b928466bd
smurf136 <apiwat.it24@mail.kmutt.ac.th>
2019-08-28 03:23:04 +0700

    [Finished]enemy rotation, movement, hit by bullet,[Wait]change type enemy to group for spawn

--- src/scenes/Map01.js
+++ src/scenes/Map01.js
@@ -7,6 +7,8 @@ let bullet;
 let bullets;
 let Bullet;
 let amount;
+let enemy;
+let life = true;
 class Map01 extends Phaser.Scene {
     constructor(test) {
         super({
@@ -18,12 +20,24 @@ class Map01 extends Phaser.Scene {
         this.load.image('bg01', 'src/assets/images/desert-backgorund-looped.png')
         this.load.spritesheet('player', 'src/assets/images/ship.png', { frameHeight: 24, frameWidth: 16 })
         this.load.spritesheet('bullet', 'src/assets/images/laser-bolts.png', { frameHeight: 16, frameWidth: 16 })
+        this.load.spritesheet('enemy', 'src/assets/images/enemy-big.png', { frameHeight: 32, frameWidth: 32 })
     }
 
     create() {
         // bg01 = this.add.tileSprite(100, 50, 600, 1000, 'bg01')
         bg01 = this.add.tileSprite(620,300,800,600,'bg01').setScale(2)
         player = this.physics.add.sprite(170, 680, 'player').setSize(1.5).setScale(1.5)
+        // enemy = this.physics.add
+        // enemy.sprite(10, 50, 'enemy').setSize(1.5).setScale(1.5)
+        enemy = this.physics.add.sprite(10, 50, 'enemy').setSize(1.5).setScale(1.5)
+        // enemy = this.physics.add.sprite(20, 50, 'enemy').setSize(1.5).setScale(1.5)
+        // enemy = this.physics.add.sprite(10, 50, 'enemy').setSize(1.5).setScale(1.5)
+        // enemy = this.physics.add.sprite(10, 50, 'enemy').setSize(1.5).setScale(1.5)
+        // enemy = this.physics.add.staticGroup()
+        // enemy.create(10, 50, 'enemy')
+
+
+        player.setCollideWorldBounds(true)
 
         Bullet = new Phaser.Class({
 
@@ -51,7 +65,7 @@ class Map01 extends Phaser.Scene {
             }
         });
 
-        bullets = this.add.group({
+        bullets = this.physics.add.group({
             classType: Bullet,
             // maxSize: amount,
             runChildUpdate: true
@@ -81,9 +95,24 @@ class Map01 extends Phaser.Scene {
             frameRate: 10,
             repeat: -1
         })
-
+        this.anims.create({
+            key: 'enemy',
+            frames: this.anims.generateFrameNumbers('enemy'),
+            frameRate: 7,
+            yoyo: false,
+            repeat: -1
+        })
+        
         cursors = this.input.keyboard.createCursorKeys()
+        enemy.anims.play('enemy')
+        this.physics.add.collider(bullets, enemy, this.hit)
 
+    }
+
+    hit(){
+        life = false;
+        enemy.setTint(0x000000);
+        enemy.destroy()
     }
 
     update() {
@@ -95,6 +124,12 @@ class Map01 extends Phaser.Scene {
             }
         }
 
+        // enemy.play('enemy', true)
+        // bullets.anims.play('fired')
+        if(life){
+            enemy.angle += 5
+            enemy.setVelocityY(40)
+        }
         if(cursors.up.isDown){
             player.setVelocityY(-100)
             player.anims.play('turn')
